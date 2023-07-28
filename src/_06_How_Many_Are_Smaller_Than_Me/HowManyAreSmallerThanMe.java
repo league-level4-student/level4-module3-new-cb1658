bed266d7fec8e23107632ce8ff0b75a776810916
league <league@imac-32.attlocal.net>
2023-07-28 16:19:37 -0700

    Level 4 FINISH

--- src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
+++ src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
@@ -1,5 +1,6 @@
 package _06_How_Many_Are_Smaller_Than_Me;
 
+import _03_Intro_to_Binary_Trees.Node;
 import _05_Intro_to_AVL_Trees.AVLNode;
 import _05_Intro_to_AVL_Trees.AVLTree;
 
@@ -16,10 +17,63 @@ public class HowManyAreSmallerThanMe {
 
     public int howManyAreSmallerThanMe(AVLTree<Integer> avlTree, int me) {
 
+    	Node<Integer> root = avlTree.getRoot();
     	
+    	if(root.getValue() < me) {
+    		return BFS(root,me) + 1;
+    	}
     	
-        return 0;
+    	else {
+    		return BFS(root,me);
+    	}
 
     }
-
+    
+    public int BFS(Node<Integer> node, int me) {
+    	Node<Integer> left = node.getLeft();
+    	Node<Integer> right = node.getRight();
+    	
+    	
+    	int smallerThanMe = 0;
+    	
+    	
+    	if(right != null && left != null && right.getValue() < me) {
+    		smallerThanMe = smallerThanMe+2;
+    	}
+    	
+    	if((right == null && left != null) ||
+    			(left == null && right != null && right.getValue() < me)) {
+    		smallerThanMe++;
+    	}
+    	
+    	
+    	
+    	
+    	
+    	if(left != null && right != null) {
+    		
+    		System.out.println(left.getValue());
+    		System.out.println(right.getValue());
+    		
+    		return smallerThanMe + BFS(left,me) + BFS(right,me);
+    		
+        	}
+    	
+    	if(left != null && right == null) {
+    		System.out.println(left.getValue());
+    		
+    		return smallerThanMe + BFS(left,me);
+    	}
+    	
+    	if(left == null && right != null) {
+    		System.out.println(right.getValue());
+    		
+    		return smallerThanMe + BFS(right, me);
+    	}
+    	
+    	else {
+    		return smallerThanMe;
+    	}
+    	
+    }
 }
